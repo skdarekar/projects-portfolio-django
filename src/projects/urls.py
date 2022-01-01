508e81a413cffd10f484f4a72ec7439ecfbbe171
Sagar Darekar <sagardarekar1997@gmail.com>
2022-01-01 22:47:10 +0530

    static assets usage, edit and details project view

--- src/projects/urls.py
+++ src/projects/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import (project_home_view, project_list_view, project_add_view)
+from .views import (project_home_view, 
+project_list_view, 
+project_add_view, 
+project_detail_view,
+project_edit_view)
 
 urlpatterns = [
     path('', project_home_view, name="project-home-view"),
     path('list/', project_list_view, name="project-list-view"),
+    path('<int:project_id>', project_detail_view, name="project-detail-view"),
+    path('<int:project_id>/update', project_edit_view, name="project-update-view"),
     path('add/', project_add_view, name="project-add-view"),
 ]
--- src/projects/views.py
+++ src/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, HttpResponseRedirect)
 from .models import Project
 from .forms import ProjectForm
 # Create your views here.
@@ -15,6 +15,23 @@ def project_list_view(request):
     }
     return render(request, 'projects/project_list.html', context)
 
+def project_edit_view(request, project_id):
+    projectObj = get_object_or_404(Project, id=project_id);
+    form = ProjectForm(request.POST or None, instance=projectObj);
+    if form.is_valid():
+        form.save();
+        return HttpResponseRedirect("/projects/list")
+    context = {
+        'project_form': form
+    }
+    return render(request, 'projects/project_edit.html', context)
+
+def project_detail_view(request, project_id):
+    projectObj = get_object_or_404(Project, id=project_id);
+    context = {
+        'projectObj': projectObj
+    }
+    return render(request, 'projects/project_detail.html', context)
 
 def project_add_view(request):
     form = ProjectForm(request.POST or None)
